b3b67629ac0b1dc6b10e69c4e6896eddb1c021c4
abdulabuaaa <40563034+abdulabuaaa@users.noreply.github.com>
2018-08-01 05:45:08 +0000

    Create digits of an integer.java

---
+++ digits of an integer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class repeat
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int w=s.length();
+        System.out.print(w);
+    }
+}

11519fea1b936f46cdd792726648ede2f68a8e6f
abdulabuaaa <40563034+abdulabuaaa@users.noreply.github.com>
2018-08-16 09:43:47 +0530

    Update even between n numbers.java

--- even between n numbers.java
+++ even between n numbers.java
@@ -5,7 +5,7 @@ class evennumbersbetweentwonumbers
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt(),m=sc.nextInt();
-        for(int i=n;i<m;i++)
+        for(int i=n+1;i<m;i++)
         {
             if(i%2==0)
             {

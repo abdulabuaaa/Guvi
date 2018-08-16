36219b056f072549db4e77fe2f308421bf8e62b1
abdulabuaaa <40563034+abdulabuaaa@users.noreply.github.com>
2018-08-16 09:37:56 +0530

    Create even between n numbers.java

---
+++ even between n numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class evennumbersbetweentwonumbers
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),m=sc.nextInt();
+        for(int i=n;i<m;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
